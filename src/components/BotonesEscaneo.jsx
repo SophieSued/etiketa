f6c62bc396e0c1d48e9c98e659863dc118fcc349
sophiesued <suedsophie550@gmail.com>
2025-06-06 14:16:46 -0300

    css botones escaneo y flashh

--- src/components/BotonesEscaneo.jsx
+++ src/components/BotonesEscaneo.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { PiRadioButtonThin } from "react-icons/pi";
+import { IoFlashOutline } from "react-icons/io5";
+import { CiImageOn } from "react-icons/ci";
+import "../styles/BotonesEscaneo.css";
+
 
 const BotonesEscaneo = () => {
   return (
     <div className="botones-escaneo">
-      <button className="boton-icono">📷</button>
-      <button className="boton-icono">📡</button>
-      <button className="boton-icono">💡</button>
+      <button className="boton-icono">
+        <CiImageOn size={50} />
+      </button>
+      <button className="boton-icono">
+        <PiRadioButtonThin size={90} />
+      </button>
+      <button className="boton-icono">
+        <IoFlashOutline size={50} />
+      </button>
     </div>
   );
 };
